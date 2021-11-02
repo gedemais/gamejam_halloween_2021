b2bb7e8fb4c1cc31e8a57dc5462c5e030ef5e9d6
jbirdqiqat <MbDBt.BN9hp@protonmail.com>
2021-11-02 17:56:30 +0100

    commit

--- player.py
+++ player.py
@@ -6,6 +6,30 @@ EAST=2
 WEST=3
 V=4
 
+potions =   {
+                "Biere dephilosophale": 0,
+                "Huile": 1,
+                "Cire d'abeille": 2,
+                "Red Bull": 3,
+                "Vodka": 4,
+                "Essence": 5,
+                "Nitroglycerine": 6,
+                "Eau Benite": 7,
+                "Eau de Galadriel": 8,
+                "Hydrophobie": 9,
+                "Potion de levitation": 10,
+                "Solution d'Eau De Feu": 11,
+                "Breuvage phosphorescent": 12,
+                "Potion de Nuage": 13,
+                "Potion d'Uranium": 14,
+                "Gelee Royale": 15,
+                "Napalm": 16,
+                "Lotion d'Ange": 17,
+                "Larmes d'Ange": 18,
+                "Hydromel": 19,
+                "Poudre d'Ange": 20,
+                "Melange douteux": 21,
+            }
 
 directions = [(0, -1), (0, 1), (1, 0), (-1, 0)]
 
@@ -41,6 +65,7 @@ class   Player:
         self.y = y
         self.load_sprites()
         self.hitbox = Hitbox(x, y, width=32, height=32)
+        self.inventory = []
 
     def load_sprites(self):
         self.top_walk, self.bot_walk, self.right_walk, self.left_walk = [], [], [], []
@@ -54,6 +79,8 @@ class   Player:
             self.left_walk.append(self.walk_ss.subsurface(x, res * 2, res, res))
             self.bot_walk.append(self.walk_ss.subsurface(x, res * 3, res, res))
             x += res
+
+    #def take_potion()
 
     def walk(self, arrows, room):
         tmp_x = self.x
